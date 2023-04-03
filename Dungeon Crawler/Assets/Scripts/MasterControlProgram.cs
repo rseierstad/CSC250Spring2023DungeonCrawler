136ba46c32efdc242fb657982f1c02db3d3c86f4
rseierstad <rachel.seierstad@cuw.edu>
2023-04-03 11:54:42 -0500

    Class 4/3

--- Dungeon Crawler/Assets/Scripts/MasterControlProgram.cs
+++ Dungeon Crawler/Assets/Scripts/MasterControlProgram.cs
@@ -1,5 +1,5 @@
 public class MasterControlProgram
 {
     public static int count = 0;
+    public static string whereDidIComeFrom = "?";
 }
-
--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -8,48 +8,62 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     public GameObject northExit, southExit, eastExit, westExit;
     public float movementSpeed = 40.0f;
-    private string movingTowards = "";
+    private bool isMoving;
 
     // Start is called before the first frame update
     void Start()
     {
         this.rb = this.GetComponent<Rigidbody>();
         print(MasterControlProgram.count);
+        this.isMoving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(movingTowards.Equals(""))
+        if(Input.GetKeyDown(KeyCode.UpArrow) && this.isMoving == false)
         {
-           if(Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                this.rb.AddForce(this.northExit.transform.position * movementSpeed);
-                movingTowards = "north";
-            }
-            if(Input.GetKeyDown(KeyCode.DownArrow))
-            { 
-                this.rb.AddForce(this.southExit.transform.position * movementSpeed);
-                movingTowards = "south";
-            }
-            if(Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                this.rb.AddForce(this.eastExit.transform.position * movementSpeed);
-                movingTowards = "east";
-            }
-            if(Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                this.rb.AddForce(this.westExit.transform.position * movementSpeed);
-                movingTowards = "west";
-            }
+            this.rb.AddForce(this.northExit.transform.position * movementSpeed);
+            this.isMoving = true;
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow) && this.isMoving == false)
+        { 
+            this.rb.AddForce(this.southExit.transform.position * movementSpeed);
+            this.isMoving = true;
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow) && this.isMoving == false)
+        {
+            this.rb.AddForce(this.eastExit.transform.position * movementSpeed);
+            this.isMoving = true;
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && this.isMoving == false)
+        {
+            this.rb.AddForce(this.westExit.transform.position * movementSpeed);
+            this.isMoving = true;
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         if(other.gameObject.CompareTag("exit"))
         {
-            MasterControlProgram.count++;
+            if(other.gameObject == this.northExit)
+            {
+                MasterControlProgram.whereDidIComeFrom = "north";
+            }
+            if(other.gameObject == this.southExit)
+            {
+                MasterControlProgram.whereDidIComeFrom = "south";
+            }
+            if(other.gameObject == this.eastExit)
+            {
+                MasterControlProgram.whereDidIComeFrom = "east";
+            }
+            if(other.gameObject == this.westExit)
+            {
+                MasterControlProgram.whereDidIComeFrom = "west";
+            }
+
             SceneManager.LoadScene("Dungeon Room");
         }
     }
