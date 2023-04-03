2044eb632d7e4a1ea0933fd39b6628998242819a
rseierstad <rachel.seierstad@cuw.edu>
2023-04-03 17:21:55 -0500

    Work on HW 11

--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -7,20 +7,70 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public GameObject northExit, southExit, eastExit, westExit;
+    public Vector3 northEntrancePosition, southEntrancePosition, eastEntrancePosition, westEntrancePosition;
+    public Vector3 playerStartPosition;
     public float movementSpeed = 40.0f;
     private bool isMoving;
+    private bool reset;
 
     // Start is called before the first frame update
     void Start()
     {
         this.rb = this.GetComponent<Rigidbody>();
-        print(MasterControlProgram.count);
+        this.playerStartPosition = new Vector3(0, 0.5f, 0);
         this.isMoving = false;
+        this.reset = true;
+
+        this.northEntrancePosition = new Vector3(this.northExit.transform.position.x, 0.5f, this.northExit.transform.position.z - 1.15f);
+        this.southEntrancePosition = new Vector3(this.southExit.transform.position.x, 0.5f, this.southExit.transform.position.z + 1.15f);
+        this.eastEntrancePosition = new Vector3(this.eastExit.transform.position.x - 1.15f, 0.5f, this.eastExit.transform.position.z);
+        this.westEntrancePosition = new Vector3(this.westExit.transform.position.x + 1.15f, 0.5f, this.westExit.transform.position.z);
+
+        if(MasterControlProgram.whereDidIComeFrom.Equals("north"))
+        {
+            MasterControlProgram.whereDidIComeFrom = "?";
+            this.rb.transform.position = this.southEntrancePosition;
+            this.rb.AddForce(new Vector3(0,0,40f));
+            this.isMoving = true;
+            this.reset = false;
+        }
+        if(MasterControlProgram.whereDidIComeFrom.Equals("south"))
+        {
+            MasterControlProgram.whereDidIComeFrom = "?";
+            this.rb.transform.position = this.northEntrancePosition;
+            this.rb.AddForce(new Vector3(0,0,-40f));
+            this.isMoving = true;
+            this.reset = false;
+        }
+        if(MasterControlProgram.whereDidIComeFrom.Equals("east"))
+        {
+            MasterControlProgram.whereDidIComeFrom = "?";
+            this.rb.transform.position = this.westEntrancePosition;
+            this.rb.AddForce(new Vector3(40f,0,0));
+            this.isMoving = true;
+            this.reset = false;
+        }
+        if(MasterControlProgram.whereDidIComeFrom.Equals("west"))
+        {
+            MasterControlProgram.whereDidIComeFrom = "?";
+            this.rb.transform.position = this.eastEntrancePosition;
+            this.rb.AddForce(new Vector3(-40f,0,0));
+            this.isMoving = true;
+            this.reset = false;
+        }
+
+        //AddForce moves it weird
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(this.rb.transform.position == this.playerStartPosition)
+        {
+            this.rb.velocity = Vector3.zero;
+            this.rb.angularVelocity = Vector3.zero;
+        } //not working
+
         if(Input.GetKeyDown(KeyCode.UpArrow) && this.isMoving == false)
         {
             this.rb.AddForce(this.northExit.transform.position * movementSpeed);
