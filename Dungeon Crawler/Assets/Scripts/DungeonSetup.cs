bb3a38eddc5bd78b8ab10a81823fe82eb9cb9547
rseierstad <rachel.seierstad@cuw.edu>
2023-04-19 13:21:35 -0500

    Adding Aesthetic Bits

--- Dungeon Crawler/Assets/Scripts/DungeonSetup.cs
+++ Dungeon Crawler/Assets/Scripts/DungeonSetup.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class DungeonSetup : MonoBehaviour
 {
     public GameObject northExit, southExit, eastExit, westExit;
+    public GameObject northWall, southWall, eastWall, westWall;
     private Room currentRoom;
 
     // Start is called before the first frame update
     void Start()
     {
         MasterControlProgram.setupDungeon();
+
+        this.northWall.SetActive(false);
+        this.southWall.SetActive(false);
+        this.eastWall.SetActive(false);
+        this.westWall.SetActive(false);
 
         this.setExits();
     }
@@ -21,18 +27,22 @@ public class DungeonSetup : MonoBehaviour
         if(!currentRoom.hasExit("north"))
         {
             this.northExit.SetActive(false);
+            this.northWall.SetActive(true);
         }
         if(!currentRoom.hasExit("south"))
         {
             this.southExit.SetActive(false);
+            this.southWall.SetActive(true);
         }
         if(!currentRoom.hasExit("east"))
         {
             this.eastExit.SetActive(false);
+            this.eastWall.SetActive(true);
         }
         if(!currentRoom.hasExit("west"))
         {
             this.westExit.SetActive(false);
+            this.westWall.SetActive(true);
         }
     }
 }
